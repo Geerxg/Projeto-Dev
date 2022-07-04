c02fc60d0562e3ce74656addd46577b500ac0964
Geerxg <geerpimp69@gmail.com>
2022-07-04 16:34:22 -0300

    Table update

--- database/migrations/2022_06_22_134556_create_customers_table.php
+++ database/migrations/2022_06_22_134556_create_customers_table.php
@@ -16,15 +16,15 @@ return new class extends Migration
     {
         Schema::create('customers', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string("document");
-            $table->string("email");
-            $table->string("postal_code");
-            $table->string("address");
-            $table->string("neighborhood");
-            $table->string("city");
-            $table->string("number");
-            $table->char("state");
+            $table->string('name',45);
+            $table->string("document",20);
+            $table->string("email",45);
+            $table->string("postal_code",15);
+            $table->string("address",100);
+            $table->string("neighborhood",45);
+            $table->string("city",45);
+            $table->string("number",15);
+            $table->char("state",2);
             $table->timestamps();
         });
     }
